b0d8d9f709ecdfff2a62ee683488ffb005a04bc0
jangwonseo <mklmkl2001@gmail.com>
2016-07-25 14:29:34 +0900

    board list complete

--- src/main/java/org/zerock/controller/BoardController.java
+++ src/main/java/org/zerock/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.domain.BoardVO;
 import org.zerock.service.BoardService;
 
@@ -15,24 +16,33 @@ import org.zerock.service.BoardService;
 @RequestMapping("/board/*")
 public class BoardController {
 	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
-	
+
 	@Inject
 	private BoardService service;
-	
-	@RequestMapping(value="/register", method=RequestMethod.GET)
-	public void registerGET(BoardVO board, Model model) throws Exception{
+
+	@RequestMapping(value = "/register", method = RequestMethod.GET)
+	public void registerGET(BoardVO board, Model model) throws Exception {
 		logger.info("register get..................................");
 	}
-	
-	@RequestMapping(value="/register",method=RequestMethod.POST)
-	public String registerPOST(BoardVO board, Model model) throws Exception{
+
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public String registerPOST(BoardVO board, RedirectAttributes rttr) throws Exception {
 		logger.info("regist post...................................");
 		logger.info(board.toString());
-		
+
 		service.regist(board);
-		
-		model.addAttribute("result","success");
-		
-		return "/board/success";
+
+		// model.addAttribute("result","success");
+		rttr.addFlashAttribute("msg", "success");
+		// return "/board/success";
+		return "redirect:/board/listAll";
 	}
+
+	@RequestMapping(value = "/listAll", method = RequestMethod.GET)
+	public void listAll(Model model) throws Exception {
+		logger.info("show all list");
+		model.addAttribute("list",service.listAll());
+	}
+	
+	
 }
--- src/main/java/org/zerock/domain/BoardVO.java
+++ src/main/java/org/zerock/domain/BoardVO.java
@@ -9,7 +9,7 @@ public class BoardVO {
 	private String content;
 	private String writer;
 	private Date regdate;
-	private int viewvnt;
+	private int viewcnt;
 	public Integer getBno() {
 		return bno;
 	}
@@ -40,16 +40,16 @@ public class BoardVO {
 	public void setRegdate(Date regdate) {
 		this.regdate = regdate;
 	}
-	public int getViewvnt() {
-		return viewvnt;
+	public int getViewcnt() {
+		return viewcnt;
 	}
-	public void setViewvnt(int viewvnt) {
-		this.viewvnt = viewvnt;
+	public void setViewcnt(int viewcnt) {
+		this.viewcnt = viewcnt;
 	}
 	@Override
 	public String toString() {
 		return "BoardVO [bno=" + bno + ", title=" + title + ", content=" + content + ", writer=" + writer + ", regdate="
-				+ regdate + ", viewvnt=" + viewvnt + "]";
+				+ regdate + ", viewvnt=" + viewcnt + "]";
 	}
 	
 
